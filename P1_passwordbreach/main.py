afcde480fa38422f51fcd8ebe2331538aecfe1fd
Lukas Bade <lukas.f.bade@gmail.com>
2026-06-01 16:29:56 +0200

    Working version, adding to main and readme
    Finalizing the functional project, possible upgrades include a ranking of how good the password is and/or how to improve it.

--- P1_passwordbreach/main.py
+++ P1_passwordbreach/main.py
@@ -13,18 +13,23 @@ def hashing(password):
 def checkforbreach(prefix, suffix):
     url = f"https://api.pwnedpasswords.com/range/{prefix}"
     response = requests.get(url)
+    #Call API, giving prefix as call parameter
 
     if response.status_code != 200:
         raise RuntimeError(f"Error fetching data: {response.status_code}")
-    
+    #Error checking
+
+
     hashes = response.text.splitlines()
+    #Splitting answer into suffixes and count of breaches (EXAMPLEHASH:1)
+
 
     for line in hashes:
         returned_suffix, count = line.split(":")
         if returned_suffix == suffix:
             return f"Found in {count} breaches."
     return "Found in 0 breaches."
-
+    #Comparing suffixes for matches, adding count upon match
 
 
 
