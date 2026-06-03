03db464e192e29b3212486a3192cfccdc7b6bb1f
Lukas Bade <lukas.f.bade@gmail.com>
2026-06-03 23:31:11 +0200

    Bugfixing regarding false token

--- P2_discordbot/main.py
+++ P2_discordbot/main.py
@@ -1,23 +1,27 @@
+from dotenv import load_dotenv
+import os
 import discord
 from discord.ext import commands
 import logging
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-token = os.getenv('DISCORD_TOKEN')
+token = os.getenv("DISCORD_TOKEN")
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+if token is None:
+    raise ValueError("DISCORD_TOKEN not loaded. Check .env path!")
+
+handler = logging.FileHandler("discord.log", encoding="utf-8", mode="w")
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
-    print('------')
+    print(f"Logged in as {bot.user}")
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
